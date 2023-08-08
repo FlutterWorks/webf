fb77031531d46367a848e9ea2fd774cde1ad861e
Storm-M <83829676@qq.com>
2023-08-08 19:57:20 +0800

    fix: fix css empty problem

--- webf/lib/src/rendering/flow.dart
+++ webf/lib/src/rendering/flow.dart
@@ -1037,6 +1037,9 @@ class RenderFlowLayout extends RenderLayoutBox {
     if (child.isScrollingContentBox) {
       return 0;
     }
+    if (child.size.isEmpty) {
+      return 0;
+    }
     return child.renderStyle.collapsedMarginTop;
   }
 
